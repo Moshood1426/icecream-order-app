f6e01a87d0c79d3645431aceb2bd86c86a63b25b
Moshood1426 <moshood.yemi10@gmail.com>
2023-01-17 04:42:37 -0500

    popover added

--- frontend/src/pages/summary/test/SummaryForm.test.js
+++ frontend/src/pages/summary/test/SummaryForm.test.js
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("initial render", () => {
@@ -12,7 +13,8 @@ test("initial render", () => {
   expect(submitBtn).toBeDisabled();
 });
 
-test("Checks if checkbox disabled button when checked", () => {
+test("Checks if checkbox disabled button when checked", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
 
   const checkbox = screen.getByRole("checkbox", {
@@ -20,9 +22,29 @@ test("Checks if checkbox disabled button when checked", () => {
   });
   const submitBtn = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(submitBtn).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(submitBtn).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  const popover = screen.getByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(popover).toBeInTheDocument();
+
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
