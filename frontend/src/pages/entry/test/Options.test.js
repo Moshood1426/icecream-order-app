ac9a03844dc32a4e989f81b0369c2e7fa92a7dfe
Moshood1426 <moshood.yemi10@gmail.com>
2023-01-18 05:32:07 -0500

    toppings options and test added

--- frontend/src/pages/entry/test/Options.test.js
+++ frontend/src/pages/entry/test/Options.test.js
@@ -1,13 +1,26 @@
 import { screen, render } from "@testing-library/react";
 import Options from "../Options";
 
-test("confirm if images are displaying correctly", async () => {
+test("confirm if scoops are displaying correctly", async () => {
   render(<Options optionType="scoops" />);
 
   const optionItems = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(optionItems).toHaveLength(2);
-  
+
   const altText = optionItems.map((item) => item.alt);
-  console.log(altText)
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("confirm if toppings are displaying correctly", async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppings = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(toppings).toHaveLength(3);
+
+  const altText = toppings.map((item) => item.alt);
+  expect(altText).toEqual([
+    "M&Ms topping",
+    "Hot fudge topping",
+    "Cherries topping",
+  ]);
 });
