179d49a7287f4692dd7ea9e0017e66b4d181bd8e
Moshood1426 <moshood.yemi10@gmail.com>
2023-01-20 04:01:04 -0500

    grand total test added

--- frontend/src/pages/entry/test/OrderEntry.test.js
+++ frontend/src/pages/entry/test/OrderEntry.test.js
@@ -1,22 +1,52 @@
-import { render, screen, waitFor } from "../../../test-utils/testing-library-utils"
+import {
+  render,
+  screen,
+  waitFor,
+} from "../../../test-utils/testing-library-utils";
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
+import userEvent from "@testing-library/user-event";
 
 test("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
     rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
     ),
-    rest.get("http://localhost:3030/scoops", (req, res, ctx) => 
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
     )
   );
 
   render(<OrderEntry />);
 
-  const alerts = await screen.findAllByRole("alert", {
-    name: "An unexpected error occured. Please try again later",
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
+
+    expect(alerts).toHaveLength(2);
   });
-  expect(alerts).toHaveLength(2)
+});
+
+test("confirm grand total", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry />);
+
+  const totalValue = screen.getByRole("heading", { name: /^Grand total:/i });
+  expect(totalValue).toHaveTextContent("0");
+
+  const scoopEntry = await screen.findByRole("spinbutton", {
+    name: /^Chocolate/i,
+  });
+  await user.clear(scoopEntry);
+  await user.type(scoopEntry, "1");
+  expect(totalValue).toHaveTextContent("2");
+
+  const toppingsEntry = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(toppingsEntry);
+  expect(totalValue).toHaveTextContent("3.5");
+
+  await user.click();
+  expect(totalValue).toHaveTextContent("2");
 });
--- frontend/src/pages/entry/test/totalUpdates.test.js
+++ frontend/src/pages/entry/test/totalUpdates.test.js
@@ -25,17 +25,23 @@ test("confirm total scoop order value after selection", async () => {
   expect(scoopItems).toHaveTextContent("6.00"); //since 1 vanilla scoop cost $2
 });
 
-// test("confirm total topping order value after selection", async () => {
-//   const user = userEvent.setup();
-//   render(<Options optionType="toppings" />, {
-//     wrapper: OrderDetailsProvider,
-//   });
+test("confirm total topping order value after selection", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="toppings" />, {
+    wrapper: OrderDetailsProvider,
+  });
 
-//   const toppingItems = screen.getByText("Toppings total: $", { exact: false });
-//   expect(toppingItems).toHaveTextContent("0.00");
+  const toppingItems = screen.getByText("Toppings total: $", { exact: false });
+  expect(toppingItems).toHaveTextContent("0.00");
 
-//   const inputEl = await screen.findByRole("spinbutton", { name: "M&Ms" });
-//   await user.clear(inputEl);
-//   await user.type(inputEl, "1");
-//   expect(toppingItems).toHaveTextContent("2.00");
-// });
+  const mInput = await screen.findByRole("checkbox", { name: "M&Ms" });
+  await user.click(mInput)
+  expect(toppingItems).toHaveTextContent("1.50");
+
+  const cherrieInput = await screen.findByRole("checkbox", { name: "Cherries" });
+  await user.click(cherrieInput)
+  expect(toppingItems).toHaveTextContent("3.00");
+
+  await user.click(mInput)
+  expect(toppingItems).toHaveTextContent("1.50");
+});
